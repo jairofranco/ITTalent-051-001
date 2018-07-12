100af0ec694e0b6420f937595262fc0949828c82
Pablo Riaño <tauorus@gmail.com>
2018-07-12 14:07:35 -0500

    Actualizacion Backend

---
+++ Backend/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :pictures
+  resources :conventions
+  resources :reports
+  resources :points
+  resources :visitors
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
